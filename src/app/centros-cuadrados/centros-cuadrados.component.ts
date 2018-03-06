97afab6d6a9e15fbd119709317fc127d6989c701
Gustavo3125 <gustavo31.25@gmail.com>
2018-03-06 11:48:46 -0600

    Listo Chi

--- src/app/centros-cuadrados/centros-cuadrados.component.ts
+++ src/app/centros-cuadrados/centros-cuadrados.component.ts
@@ -12,9 +12,14 @@ export class CentrosCuadradosComponent implements OnInit {
   
   seedText = '';
   numbersToGenerateText = '';
-
+  chi: number[][] =[[3.8415, 5.9915,7.8147,9.4877,11.0705,12.5916,14.0671,15.5073,16.9190,
+  18.3070,19.6752, 21.0261, 22.3620, 23.6848, 24.9958, 26.2962,27.5871, 28.8693, 30.1435, 
+  31.4104, 32.6706],[2.7055, 4.6052, 6.2514, 7.7794, 9.2363,10.6446,12.0170, 13.3616, 14.6837, 15.9872, 12.2750, 18.5493,
+  19.8119, 21.0641, 22.3071, 23.5418, 24.7690, 24.1555, 25.3289, 26.4976,27.662]]
+  finalcompare:number;
   private seedNumber: number;
   private numbersToGenerateNumber: number;
+  private selected:number;
   generatedRandomNumbers: number[] = [];
 
   onShowFlashMessage(text: string, success: boolean) {
@@ -96,12 +101,13 @@ export class CentrosCuadradosComponent implements OnInit {
       let count =0;
       while(r==false&&count<kin){
         if(arreglados[n]<=tablas[count]){
-            fabs[count]++;
+            fabs[count]=fabs[count]+1;
             r=true;
         }else{
           count++;
         }
       }
+      
     }
     for(let n =0; n<kin; n++){
       if(fabs[n]<5){
@@ -122,6 +128,26 @@ export class CentrosCuadradosComponent implements OnInit {
       final.push(Math.pow(fersteorica[newn]-fers[newn],2)/fersteorica[newn]);
     }
 
+    final.forEach(element => {
+      this.finalcompare=element+this.finalcompare;
+      
+    });
+    v=k-1;
+    if(this.selected==0.05){
+      if(this.finalcompare<this.chi[0][v-1]){
+        this.onShowFlashMessage(this.finalcompare + ' Pasa la prueba pues Es menor que el valor chi:' +this.chi[0][v-1], true);
+      }else{
+        this.onShowFlashMessage(this.finalcompare + ' No Pasa la prueba pues Es menor que el valor chi:' +this.chi[0][v-1], true);
+      }
+    }else{
+      if(this.finalcompare<this.chi[1][v-1]){
+        this.onShowFlashMessage(this.finalcompare + ' Pasa la prueba pues Es menor que el valor chi:' +this.chi[1][v-1], true);
+      }else{
+        this.onShowFlashMessage(this.finalcompare + ' No Pasa la prueba pues Es menor que el valor chi:' +this.chi[1][v-1], true);
+      }
+    }
    
   }
+
+
 }
