84e10cb18c9b3d776154938b1dfb63c1d67a7146
Gustavo3125 <gustavo31.25@gmail.com>
2018-03-07 23:00:59 -0600

    Ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
+import { FlashMessageComponent } from './flash-message/flash-message.component';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { CentrosCuadradosComponent } from './centros-cuadrados/centros-cuadrados.component';
-import { CongruencialComponent } from './congruencial/congruencial.component';
-import { CongruencialMixtoComponent } from './congruencial-mixto/congruencial-mixto.component';
 import { GeneradorMultiplicativoComponent } from './generador-multiplicativo/generador-multiplicativo.component';
-import { FlashMessageComponent } from './flash-message/flash-message.component';
+import { CentrosCuadradosComponent } from './centroscuadrados/centros-cuadrados.component';
+import { CongruencialMixtoComponent } from './congruencialmixto/congruencial-mixto.component';
+import { CongruencialComponent } from './congruencial/congruencial.component';
 
 @NgModule({
   declarations: [
